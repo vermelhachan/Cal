c698aa2063da394bdc7fe7dccad0ceb1ad88ac3e
vermelhachan <71857226+vermelhachan@users.noreply.github.com>
2020-10-11 22:58:42 -0300

    Update and rename README.md to forma.c

---
+++ forma.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+   int base;
+       int altura;
+        int area;
+printf("Digite a base:\n");
+scanf("%i",&base);
+printf("Digite a altura:\n");
+scanf("%i",&altura);
+area = base * altura;
+printf("O retângulo tem a area de: %i",area);
+
+
+     return 0;
+}
